fd6a06796e6986f195a2c38405c035aba5aafdd5
KeXiaohao <2239830393@qq.com>
2024-02-16 00:02:51 +0800

    adjust the ui of time setting card

--- app/src/main/java/com/example/mediation/ui/components/NumberPickerWrapper.kt
+++ app/src/main/java/com/example/mediation/ui/components/NumberPickerWrapper.kt
@@ -4,7 +4,11 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
@@ -13,8 +17,13 @@ import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.*
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,7 +51,12 @@ fun NumberPickerWrapper(
             range = range,
             dividersColor = Color.Transparent,
             label = { "${if (abs(it) < 10) "0" else ""}$it" },
-            textStyle = TextStyle(color = icon_dark_color)
+            textStyle = TextStyle(
+                color = icon_dark_color,
+                fontFamily = FontFamily.Monospace,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold
+            )
         )
         val textMeasurer = rememberTextMeasurer()
         Canvas(modifier = Modifier.matchParentSize()) {
